1d0e08ec82df378c26381ea5d62af874a5dfdcef
Farcy-Corentin <corentinfarcy1@gmail.com>
2023-01-07 17:18:54 +0100

    chore(LPP-4): create header

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-const container = document.createElement('div')
-container.classList.add('container')
+import Header from './src/views/components/Header.js'
 
-const title = document.createElement('h1')
-title.textContent = 'Les petits plats'
-
-container.appendChild(title)
-
-document.querySelector('#app').appendChild(container)
+document.querySelector('#app').appendChild(Header())
